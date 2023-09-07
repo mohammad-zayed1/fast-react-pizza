48bd75e8d8573775c35bf10c830d289c8fa6b915
mohammad-zayed1 <mohammadzayed193@gmail.com>
2023-09-07 18:15:42 +0300

    setup the project with no styles yet

---
+++ src/features/order/OrderItem.jsx
@@ -0,0 +1,19 @@
+/* eslint react/prop-types: 0 */
+import { formatCurrency } from "../../utils/helpers";
+
+function OrderItem({ item, isLoadingIngredients, ingredients }) {
+  const { quantity, name, totalPrice } = item;
+
+  return (
+    <li>
+      <div>
+        <p>
+          <span>{quantity}&times;</span> {name}
+        </p>
+        <p>{formatCurrency(totalPrice)}</p>
+      </div>
+    </li>
+  );
+}
+
+export default OrderItem;
